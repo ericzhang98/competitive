c5bcb7b32a5c1665fb417108e8afac13e6d85664
Eric Zhang <zhangeric99@gmail.com>
2020-11-27 19:12:55 -0800

    better templating

--- codeforces/archive.sh
+++ codeforces/archive.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 archive=$1
+if [ -z "$1" ]; then
+  echo "no argument supplied"
+fi
 if [ -d $archive ]; then
-    echo $archive folder already exists, exiting...
+    echo "$archive folder already exists, exiting..."
     exit
 fi
 
@@ -11,14 +14,14 @@ mkdir $archive
 for c in {a..f}
 do
     if [ -f $c.cpp ]; then
-        echo moving $c.cpp to $archive
+        echo "moving $c.cpp to $archive"
         mv $c.cpp $archive
     fi
 done
 for c in {a..f}
 do
     if [ -f test_$c ]; then
-        echo moving test_$c to $archive
+        echo "moving test_$c to $archive"
         mv test_$c $archive
     fi
 done
--- codeforces/setup.sh
+++ codeforces/setup.sh
@@ -1,21 +1,35 @@
 #!/bin/bash
 
-echo Setting up files
+folder=$1
+templates=templates
+
+if [ -z "$1" ]; then
+  echo "No folder name provided, exiting..."
+  exit 1
+fi
+if [ -d $folder ]; then
+  echo "$folder already exists, exiting..."
+  exit 1
+fi
+
+echo "Setting up files in $folder"
+mkdir $folder
+cp $templates/Makefile $folder
 for c in {a..f}
 do
-    if [ -f $c.cpp ]; then
-        echo $c.cpp exists
-    else
-        echo Creating $c.cpp
-        cp template_cpp.cpp $c.cpp
-    fi
+  if [ -f $folder/$c.cpp ]; then
+    echo "$folder/$c.cpp exists"
+  else
+    echo "Creating $folder/$c.cpp"
+    cp $templates/cpp.cpp $folder/$c.cpp
+  fi
 done
 for c in {a..f}
 do
-    if [ -f test_$c ]; then
-        echo test_$c exists
-    else
-        echo Creating test_$c
-        touch test_$c
-    fi
+  if [ -f $folder/test_$c ]; then
+    echo "$folder/test_$c exists"
+  else
+    echo "Creating $folder/test_$c"
+    touch $folder/test_$c
+  fi
 done
